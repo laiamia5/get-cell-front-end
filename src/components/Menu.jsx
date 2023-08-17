1dd3d5f9a825dc4e1b4182030899730921f5ecdc
Laia Mia Perez <laiperez1020@gmail.com>
2023-08-17 18:39:38 -0300

    icons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '../styles/menu.css'
 import home from '../tools/imgs/home.svg'
-import mensaje from '../tools/imgs/mensaje2.svg'
+import mensaje from '../tools/imgs/mensaje3.svg'
 
 export default function Menu (){
 
