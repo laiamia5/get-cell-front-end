4207bfe0753d048d34f95bf238ecec179177144d
Laia Mia Perez <laiperez1020@gmail.com>
2023-08-17 18:33:03 -0300

    emojis navtab

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import '../styles/menu.css'
+import home from '../tools/imgs/home.svg'
+import mensaje from '../tools/imgs/mensaje1.svg'
 
 export default function Menu (){
 
@@ -36,18 +38,20 @@ export default function Menu (){
       <ul class="main-tabs">
         <li>
           <button class="round-button"  data-translate-value="300%" data-color="purple">
-            <svg fill="currentColor" viewBox="0 0 16 16">
+            <img src={home} alt="" />
+            {/* <svg fill="currentColor" viewBox="0 0 16 16">
               <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-            </svg>
+            </svg> */}
           </button>
         </li>
         
         <li>
           <button class="round-button gallery active"  data-translate-value="100%" data-color="blue">
-            <svg fill="currentColor" viewBox="0 0 16 16">
+            <img src={mensaje} alt="" />
+            {/* <svg fill="currentColor" viewBox="0 0 16 16">
               <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
               <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z" />
-            </svg>
+            </svg> */}
           </button>
         </li>
         <li>
