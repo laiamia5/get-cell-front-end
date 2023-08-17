38f81b90bec0e52a7517e9e929200bdbbb074b02
Laia Mia Perez <laiperez1020@gmail.com>
2023-08-17 18:17:58 -0300

    menutab ajustes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -35,12 +35,13 @@ export default function Menu (){
     <div class="main-tabs-wrapper">
       <ul class="main-tabs">
         <li>
-          <button class="round-button" data-translate-value="0" data-color="red">
-            <span class="avatar">
-              <img src="https://images.pexels.com/photos/3756985/pexels-photo-3756985.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="user avatar" />
-            </span>
+          <button class="round-button"  data-translate-value="300%" data-color="purple">
+            <svg fill="currentColor" viewBox="0 0 16 16">
+              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
+            </svg>
           </button>
         </li>
+        
         <li>
           <button class="round-button gallery active"  data-translate-value="100%" data-color="blue">
             <svg fill="currentColor" viewBox="0 0 16 16">
@@ -58,17 +59,17 @@ export default function Menu (){
           </button>
         </li>
         <li>
-          <button class="round-button"  data-translate-value="300%" data-color="purple">
-            <svg fill="currentColor" viewBox="0 0 16 16">
-              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
-            </svg>
-          </button>
-        </li>
-        <li>
           <button class="round-button"  data-translate-value="400%" data-color="orange">
             <svg fill="currentColor" viewBox="0 0 16 16">
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
             </svg>
+          </button>
+        </li>
+        <li>
+          <button class="round-button" data-translate-value="0" data-color="red">
+            <span class="avatar">
+              <img src="https://images.pexels.com/photos/3756985/pexels-photo-3756985.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="user avatar" />
+            </span>
           </button>
         </li>
       </ul>
